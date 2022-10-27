5c81f07f8d5370e77029ef2a1b701f7fdd964d1c
Aether Kerstens <philippekerstens@gmail.com>
2022-10-27 08:59:01 -0700

    Write student spec

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,34 @@
 require './lib/student'
 
 RSpec.describe Student do
-end
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21})
+  end
+  it '#initialize' do
+    expect(@student).to be_an_instance_of(Student)
+  end
+
+  it 'has a name' do
+    expect(@student.name).to eq("Morgan")
+  end
+
+  it 'has an age' do
+    expect(@student.age).to eq(21)
+  end
+
+  it 'keeps scores' do
+    expect(@student.scores).to eq([])
+  end
+
+  it 'can add scores' do
+    @student.log_score(89)
+    @student.log_score(78)
+    expect(@student.scores).to eq([89, 78])
+  end
+
+  it 'can calculate grade' do
+    @student.log_score(89)
+    @student.log_score(78)
+    expect(@student.grade).to eq(83.5)
+  end
+end #Average of all the scores
