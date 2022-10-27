b4037fb3d670bc8204a050654673767c1edab60d
Aether Kerstens <philippekerstens@gmail.com>
2022-10-27 08:59:13 -0700

    Write student class

---
+++ lib/student.rb
@@ -0,0 +1,15 @@
+class Student
+  attr_reader :name, :age, :scores
+
+  def initialize(student_data)
+    @name = student_data[:name]
+    @age = student_data[:age]
+    @scores = []
+  end
+  def log_score(score)
+    @scores << score
+  end
+  def grade
+    @scores.sum.to_f/@scores.length.to_f
+  end
+end
